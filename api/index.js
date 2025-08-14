f3e6e4a97b69e26671ba81bcf36a4b31c9999a5b
mohanadzeyara <mzeyara752.2@gmail.com>
2025-08-14 21:13:11 +0200

    last commit

--- api/index.js
+++ api/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
+const app = express();
 const ConnectDB = require('../config/connectDB');
 const UserRoutes = require('../Routes/UserRoutes');
 const ProductRoutes = require('../Routes/ProductRoutes');
 const cors = require('cors');
+require('dotenv').config();
 
-const app = express();
+async function initDB() {
+  await ConnectDB();
+}
 
 app.use(express.json());
 app.use('/uploads', express.static(__dirname + '/../uploads'));
 
 const corsOptions = {
-  origin: process.env.FonrtURL,
+  origin: process.env.FonrtURL || '*',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
@@ -23,6 +27,6 @@ app.use('/product', ProductRoutes);
 app.use((req, res) => res.status(404).send('Not found'));
 
 module.exports = async (req, res) => {
-  await ConnectDB();
+  await initDB();
   return app(req, res);
 };
