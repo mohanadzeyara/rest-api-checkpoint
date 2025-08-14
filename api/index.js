98c139b214cc161f8de8cd1f401bb227d853439f
mohanadzeyara <mzeyara752.2@gmail.com>
2025-08-14 21:00:24 +0200

    Move API to serverless /api for Vercel

--- api/index.js
+++ api/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const ConnectDB = require('./config/connectDB');
-const UserRoutes = require('./Routes/UserRoutes');
-const ProductRoutes = require('./Routes/ProductRoutes');
+const ConnectDB = require('../config/connectDB');
+const UserRoutes = require('../Routes/UserRoutes');
+const ProductRoutes = require('../Routes/ProductRoutes');
 const cors = require('cors');
 
 const app = express();
 
-
 app.use(express.json());
-app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use('/uploads', express.static(__dirname + '/../uploads'));
 
 const corsOptions = {
   origin: process.env.FonrtURL,
@@ -18,14 +17,12 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-
 app.use('/user', UserRoutes);
 app.use('/product', ProductRoutes);
 
 app.use((req, res) => res.status(404).send('Not found'));
 
-
 module.exports = async (req, res) => {
-  await ConnectDB(); 
+  await ConnectDB();
   return app(req, res);
 };
